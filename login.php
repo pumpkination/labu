b87196022b8c3fce75d01cb6dbdea7038afb0ae0
pumpkination <silih7pumpkin@gmail.com>
2017-04-11 20:13:45 +0900

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body background="images/bgl.png">
+<form action="login_proses.php" method="post">
+  <center>Username
+    <p><input type="text" name="username" placeholder="username"></p>
+    <p>Password</p>
+    <p><input type="password" name="password" placeholder="password"></p>
+	*Dapatkan username dan password dari Pembina PMR<br><br>
+    <br><button class="button orange" type="submit" name="submit">Login</button></center>
+</form>
+<br><br><br>
+<center><a href="main.html" class="button orange">Kembali ke Beranda</a></center>
+</body>
